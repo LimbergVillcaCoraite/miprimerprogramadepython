ca504c75fcafccaab17a88f3a46322e12a36761e
LimbergVC <limbervillcacoraite@gmail.com>
2024-08-17 16:01:18 -0400

    otro

---
+++ calculadora.py
@@ -0,0 +1,6 @@
+numero_uno = 1
+numero_dos = 2
+
+#suma = numero_uno / numero_dos
+#print(suma)
+print(numero_uno + numero_dos, numero_uno - numero_dos, sep="      ")#comen
---
+++ miprimerprograma.py
@@ -0,0 +1,14 @@
+nombre = "juan pablo"
+print(type(nombre));
+#str -> string
+
+numero = 1
+print(type(numero));
+
+nombre = True #false
+print(type(nombre));
+
+numero = 12.5
+print(type(numero));
+#float - double;
+#operadores basico de python = +, -, *, /
